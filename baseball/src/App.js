747b6283d73077cb01690f1e972d1559460fb48c
Tristan Grovender <therealtristangrovender@gmail.com>
2019-05-15 15:35:12 -0500

    Display and Dashboard components fully functional

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -19,6 +19,47 @@ class App extends Component {
       };
     });
   };
+
+  strikeIncrease = e => {
+    this.setState(prevState => {
+      return {
+        strikes: prevState.strikes + 1
+      };
+    });
+  };
+
+  hitButton = e => {
+    this.setState({
+      balls: 0,
+      strikes: 0
+    });
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (this.state.balls > 3 || this.state.strikes > 2) {
+      this.setState({
+        balls: 0,
+        strikes: 0
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Display
+          ballCount={this.state.balls}
+          strikeCount={this.state.strikes}
+        />
+        <Dashboard
+          ballAction={this.ballIncrease}
+          strikeAction={this.strikeIncrease}
+          foulAction={this.ballIncrease}
+          hitAction={this.hitButton}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
